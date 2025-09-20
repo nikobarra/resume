f1eeb44a6d321e1d1d1d4ae4d663846aa11b246f
Nicolas Barra Pelecano <nicolasbarrapelecano@gmail.com>
2025-09-20 11:26:58 -0300

    feat: implement vercel speed insights and web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { LanguageProvider } from "../contexts/LanguageContext";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
             >
                 <LanguageProvider>{children}</LanguageProvider>
                 <SpeedInsights />
+                <Analytics />
             </body>
         </html>
     );
